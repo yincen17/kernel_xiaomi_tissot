76ac3c234eda9efb7fc121083877bffb20363c7a
Busetdah <apriantowpj@gmail.com>
2020-08-21 22:43:33 +0700

    defconfig: Regenerate

--- fs/f2fs/dir.c
+++ fs/f2fs/dir.c
@@ -833,16 +833,7 @@ int f2fs_fill_dentries(struct dir_context *ctx, struct f2fs_dentry_ptr *d,
 
 		/* check memory boundary before moving forward */
 		bit_pos += GET_DENTRY_SLOTS(le16_to_cpu(de->name_len));
-		if (unlikely(bit_pos > d->max ||
-				le16_to_cpu(de->name_len) > F2FS_NAME_LEN)) {
-			f2fs_warn(sbi, "%s: corrupted namelen=%d, run fsck to fix.",
-				  __func__, le16_to_cpu(de->name_len));
-			set_sbi_flag(sbi, SBI_NEED_FSCK);
-			err = -EFSCORRUPTED;
-			goto out;
-		}
-
-		if (IS_ENCRYPTED(d->inode)) {
+		if (unlikely(bit_pos > d->max)) {
 			f2fs_msg(F2FS_I_SB(d->inode)->sb, KERN_WARNING,
 				"%s: corrupted namelen=%d, run fsck to fix.",
 				__func__, le16_to_cpu(de->name_len));
